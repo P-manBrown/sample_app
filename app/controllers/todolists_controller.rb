3907b3d2fef3d0cbbd022a261185052dc1746732
P-manBrown <tachikawa.yuya@outlook.jp>
2021-11-14 01:25:16 +0000

    [Finish] 2-4

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,5 +1,21 @@
 class TodolistsController < ApplicationController
   def new
-    ＠List = List.new
+    @list = List.new
   end
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
+
+
+
+private
+def list_params
+  params.require(:list).permit(:title, :body)
 end
+
+end
+
+
+
