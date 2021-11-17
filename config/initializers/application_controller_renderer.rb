f79471b8d49bc5077aa08fffffb2d03308209c0e
P-manBrown <tachikawa.yuya@outlook.jp>
2021-11-17 02:06:36 +0000

    [Finish] 2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a5e5a18c5f51f2176af8d80908c9634e8a6e8eda2cdd7e0002432d1707db130ec53a2af654f8b371f82b271fdd01aea4ed139cafb7f870edd24fd5ab1230bf83'
